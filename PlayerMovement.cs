aacc3ca597cac2c3f22336836cf11df7c3d373af
nick <nickpuckdee@gmail.com>
2023-10-15 14:42:01 -0700

    change

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5.0f; // Adjust the movement speed as needed
+
+    private CharacterController characterController;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get input from the player
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate the movement direction based on the player's input
+        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+
+        if (moveDirection != Vector3.zero)
+        {
+            // Rotate the player to face the movement direction
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+        }
+
+        // Apply movement
+        characterController.SimpleMove(moveDirection.normalized * moveSpeed);
     }
 }
+
+
